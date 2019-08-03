85513dfa8114061f7f258e2d51a82f6b1356b803
Zalak Patel <zalakvp28@gmail.com>
2019-08-02 20:45:38 -0400

    updated

---
+++ git-maven-java/src/main/java/com/git/java/Application.java
@@ -0,0 +1,11 @@
+package com.git.java;
+
+public class Application 
+{
+    public static void main(String[] args)
+    {
+		// TODO Auto-generated method stub
+       System.out.println("Hello Zalak, Welcome to Github..!!");
+	}
+
+}
